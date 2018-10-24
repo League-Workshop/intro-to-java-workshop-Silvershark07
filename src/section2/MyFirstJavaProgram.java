8eb80e6a1a2ea56e0d2ec6775e4e0a261a04edee
munchman <nootnoot@gmail.com>
2018-10-23 19:52:25 -0700

    Day 2
    It Cool.

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot apple = new Robot();
+	apple.penDown();
+	apple.move(100);
+	apple.sparkle();
+	apple.moveTo(45, 120);
 	
-		
+
+
+	
 		
 	}
 }
