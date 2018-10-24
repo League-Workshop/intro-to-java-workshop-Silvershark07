6f7991bcfb65fab3f4045015d7cd71d29b71477a
munchman <nootnoot@gmail.com>
2018-10-23 19:54:10 -0700

    Day 2

--- src/section2/TriangleShell.java
+++ src/section2/TriangleShell.java
@@ -5,6 +5,7 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
+	Robot sam = new Robot();
 
 	
 	void go() {
@@ -28,7 +29,8 @@ public class TriangleShell {
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
 	private void drawTriangle(int length) {
-		
+		for(int i=0; i<3;i++)
+			
 		
 		
 	}
