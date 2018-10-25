95fbf874707bfe76567b01d1d62983c5f81961c1
Silvershark07 <league@WTS5.attlocal.net>
2018-10-24 19:53:56 -0700

    Day 3

--- src/section3/StephenHawking.java
+++ src/section3/StephenHawking.java
@@ -7,12 +7,15 @@ import com.sun.speech.freetts.VoiceManager;
 public class StephenHawking {
 	
 	// 1. make a main method and put steps 2, 3 & 4 inside it
+	public static void main(String[] args) {
+		
 	
 	// 4. Use a for loop to repeat steps #2 and #3, a lot of times
 	
 	// 2. ask the user for a sentence
-	
+	String sentance = JOptionPane.showInputDialog("Type a sentance");
 	// 3. call the speak method below and send it the sentence
+	speak(sentance);
 
 	
 
@@ -21,7 +24,7 @@ public class StephenHawking {
 	
 	
 	
-	
+	}
 	
 	/* Don’t change this…. */
 	static void speak(String words) {
